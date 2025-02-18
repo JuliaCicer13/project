f535dd380eea8d7294b609ca3e6c17323b6846a7
JuliaCicer13 <137887271+JuliaCicer13@users.noreply.github.com>
2025-02-18 02:59:14 +0200

    fh

--- src/js/aboutMe.js
+++ src/js/aboutMe.js
@@ -1,3 +1,37 @@
-export const messageFunctionAB = function(){
-    console.log("this is about me");
-};
+document.querySelectorAll('.container-button, .container-button-next').forEach(button => {
+    button.addEventListener('click', function() {
+        const content = this.nextElementSibling;
+        const isOpen = this.getAttribute('aria-expanded') === 'true';
+
+        document.querySelectorAll('.target-text').forEach(item => {
+            item.style.display = 'none';
+            item.previousElementSibling.setAttribute('aria-expanded', 'false');
+        });
+
+      
+        if (!isOpen) {
+            content.style.display = 'block';
+            this.setAttribute('aria-expanded', 'true');
+        }
+    });
+});
+
+
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 6,
+    spaceBetween: 0,
+    loop: true,
+    looperSlides: 20,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    keyboard: {
+        enabled: true,
+    },
+});
+
+const nextButton = document.querySelector('.swiper-button-next');
+nextButton.addEventListener('click', function() {
+    swiper.slideNext(); 
+});
