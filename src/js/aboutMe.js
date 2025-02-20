798d69312e800c5368a17a02919100f88bd8e0a4
JuliaCicer13 <137887271+JuliaCicer13@users.noreply.github.com>
2025-02-20 02:16:45 +0200

    f

--- src/js/aboutMe.js
+++ src/js/aboutMe.js
@@ -1,32 +1,27 @@
-document.querySelectorAll('.container-button, .container-button-next').forEach(button => {
-    button.addEventListener('click', function() {
-        const content = this.nextElementSibling;
-        const isOpen = this.getAttribute('aria-expanded') === 'true';
-
-        document.querySelectorAll('.target-text').forEach(item => {
-            item.style.display = 'none';
-            item.previousElementSibling.setAttribute('aria-expanded', 'false');
-        });
-
-      
-        if (!isOpen) {
-            this.setAttribute('aria-expanded', 'true');
-        }
+document.addEventListener('DOMContentLoaded', function () {
+    const swiper = new Swiper('.swiper-container', {
+        slidesPerView: 5,
+        spaceBetween: 0,
+        loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        keyboard: {
+            enabled: true,
+        },
     });
-});
 
+    const button = document.querySelector('.button');
+    const content = document.querySelector('.target-text');
 
-const swiper = new Swiper('.swiper-container', {
-    slidesPerView: 5,
-    spaceBetween: 0,
-    loop: true,
-    loopedSlides: 10,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: null,
-    },
-    keyboard: {
-        enabled: true,
-    },
+    
+    button.addEventListener('click', togleFunction);
+    function togleFunction(event) {
+        event.preventDefault(); 
+ 
+        content.classList.toggle('vissible');
+       
+     }          
 });
 
